3c1ed4a6619ab17370a0f26f60092c69b7871af0
LawlessHeaven <robert.grants97@gmail.com>
2019-06-17 23:42:05 +0300

    added client

--- client/docker/entrypoint.sh
+++ client/docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 if [ -d "./node_modules" ]; then
   # Control will enter here if $DIRECTORY exists.
   echo "Skipping 'npm install' because 'node_modules' already exists. Delete 'node_modules' and restart the container if you want to reinstall binaries."
