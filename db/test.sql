6b3dab457fd379e1535629d9c92b99c4a95a2e38
Robert Grants <robert.grants97@gmail.com>
2019-06-19 19:12:04 +0300

    Update test.sql

--- db/test.sql
+++ db/test.sql
@@ -40,33 +40,33 @@ CREATE TABLE `test_table` (
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
 (1, '2019-06-19 11:26:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(2, '2019-06-19 11:26:00');
+(2, '2019-06-19 11:27:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(3, '2019-06-19 11:26:00');
+(3, '2019-06-19 11:28:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(4, '2019-06-19 11:26:00');
+(4, '2019-06-19 11:29:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(5, '2019-06-19 11:26:00');
+(5, '2019-06-19 11:30:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(6, '2019-06-19 11:26:00');
+(6, '2019-06-19 11:31:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(7, '2019-06-19 11:26:00');
+(7, '2019-06-19 11:32:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(8, '2019-06-19 11:26:00');
+(8, '2019-06-19 11:33:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(9, '2019-06-19 11:26:00');
+(9, '2019-06-19 11:34:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(10, '2019-06-19 11:26:00');
+(10, '2019-06-19 11:35:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(11, '2019-06-19 11:26:00');
+(11, '2019-06-19 11:36:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(12, '2019-06-19 11:26:00');
+(12, '2019-06-19 11:37:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(13, '2019-06-19 11:26:00');
+(13, '2019-06-19 11:38:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(14, '2019-06-19 11:26:00');
+(14, '2019-06-19 11:39:00');
 INSERT INTO `test_table` (`id`, `date_time`) VALUES
-(15, '2019-06-19 11:26:00');
+(15, '2019-06-19 11:40:00');
 --
 -- Indexes for dumped tables
 --
