40a9d184a1b2577091159f965484dadc882c7fa8
Robert Grants <robert.grants97@gmail.com>
2019-07-11 14:30:43 +0300

    Update test.sql

--- db/test.sql
+++ db/test.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Jun 19, 2019 at 01:32 PM
--- Server version: 5.7.24-log
--- PHP Version: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
